4e7aceee9aab5e287911c23adc3aceae6ce2d7f8
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-09-05 19:11:32 -0400

    Landscaper App: user can use scissors to cut grass

---  landscaper.py
+++  landscaper.py
@@ -5,6 +5,7 @@ game_data = {
 
 ## Make $1 cutting lawns using your teeth
 def use_teeth():
+    while(True):
         user_input = int(input("""
                                 What would you like to do?
                                 [1] Cut grass using teeth
@@ -13,42 +14,87 @@ def use_teeth():
         if (user_input == 1):
             game_data["money"] += 1
             print(f"You cut the grass using teeth and made $1. You have a total of ${game_data['money']}.")
-            use_teeth()
+           if game_data["money"] >= 5:
+                buy_rusty_scissors()
 
-        if (user_input == 2):
+       else if (user_input == 2):
             game_data["quit"] = True
-
-        if (game_data["quit"] == True):
             print("You quit the game")
+else:
+            print("Invalid input. Please choose a valid option.")
 
-def rusty_scissors():
-    user_input = int(input("""
-                     What would you like to do?
-                    [1] Buy a pair of rusty scissors for $5
-                    [2] Keep cutting with teeth
-                    [3] Quit the Game 
-                    """))
-    if(user_input == 1):
-        game_data["money"] -= 5
-        print(f"You have purchased a pair of rusty scissors for $5 and now have ${game_data['money']}.")
-        use_rusty_scissors()
-
-    if(user_input == 2):
-        game_data["money"] +=1
-        use_teeth()
-
-    if(game_data["quit"] == True):
-        print("You quit the game")
-
-def use_rusty_scissors()
+def buy_rusty_scissors()
     while(True):
         user_input = int(input(""" 
                         Would you like to buy a pair of rusty scissors for $5?
                         [1] Yes, my teeth could use a break
-                        [2] No, imma keep going 
+                        [2] No
                         [3] Quit the Game
                         """))
-    if(user_input == 1):
-        game_data["money"]
+     if(user_input == 1):
 
-        //hello
+            game_data["money"] -= 5
+
+            print(f"You have purchased a pair of rusty scissors for $5 and now have ${game_data['money']}.")
+
+            use_rusty_scissors()
+
+        if(user_input == 2):
+
+            game_data["money"] +=1
+
+            use_teeth()
+
+        elif (user_input == 2):
+
+            game_data["quit"] = True
+
+        elif(game_data["quit"] == True):
+
+            print("You quit the game")
+
+
+
+
+def use_rusty_scissors():
+
+    while(True):
+
+        user_input = int(input("""
+
+                            Would you like to use your rusty scissors?
+
+                            [1] Yes
+
+                            [2] No, I want to go back to using my teeth
+
+                            [3] Quit the Game
+
+                            """))
+
+        if(user_input == 1):
+
+            game_data["money"] += 5
+
+            print(f"You cut the grass using rusty scissors and made $5. You have a total of ${game_data['money']}.")
+
+            use_rusty_scissors()
+
+
+        else if(user_input == 2):
+
+            game_data["money"] += 1
+
+            print(f"You cut the grass using teeth and made $1. You have a total of ${game_data['money']}.")
+
+            use_teeth()
+
+
+        else if(user_input ==3): 
+
+            game_data["quit"] == True
+
+            print("You quit the game")
+
+            break
+use_teeth()
